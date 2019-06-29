a4f25447c19835813ec446ef229003bb68ce19a2
MW-Potential <mwu2021@gmail.com>
2019-06-28 18:38:06 -0700

    Finished newedenfaces tutorial.

---
+++ newedenfaces/app/actions/FooterActions.js
@@ -0,0 +1,22 @@
+import alt from '../alt';//import an instance of alt, not the Alt module in the npm directories itself
+
+class FooterActions {
+    constructor() {
+        this.generateActions(
+            'getTopCharactersSuccess',
+            'getTopCharactersFail'
+        );
+    }
+
+    getTopCharacters() {
+        $.ajax({ url: '/api/characters/top'})
+            .done((data) => {
+                this.actions.getTopCharactersSuccess(data)
+            })
+            .fail((jqXhr) => {
+                this.actions.getTopCharactersFail(jqXhr)
+            });
+    }
+}
+
+export default alt.createActions(FooterActions);
